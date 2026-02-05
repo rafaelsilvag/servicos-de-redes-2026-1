e9909100c34f207c2e3af8c1ca2ee163ad8ba265
rafaelsilvag <rafaelsg@gmail.com>
2026-02-05 20:50:33 -0300

    fix: add r2 and r3 at the running script

--- topo2/run.sh
+++ topo2/run.sh
@@ -3,4 +3,6 @@
 pkill java
 java -jar ../rtr.jar routers h1-hw.txt h1-sw.txt &
 java -jar ../rtr.jar routers r1-hw.txt r1-sw.txt &
+java -jar ../rtr.jar routers r2-hw.txt r2-sw.txt &
+java -jar ../rtr.jar routers r3-hw.txt r3-sw.txt &
 java -jar ../rtr.jar routers h2-hw.txt h2-sw.txt &
